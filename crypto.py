8bf40d26c52971246d896b0018f4668f435b6dcd
MSProgrammerPy <None>
2021-04-06 15:26:34 +0430

    Add 'encrypt' function.

--- crypto.py
+++ crypto.py
@@ -6,10 +6,33 @@ class Crypto:
 
     def __init__(self, file: str):
         name, ext = os.path.splitext(file)
-        self.file_name = name
-        self.file_extension = ext
+        self.file = file
+        # File Name
+        self.name = name
+        # File Extension
+        self.extension = ext
         self._key = None
 
     def _generate_key(self):
         key = Fernet.generate_key()
         self._key = key
+
+    def encrypt(self):
+        """
+        Encrypt file
+        """
+
+        # Generating a key
+        self._generate_key()
+        fernet = Fernet(self._key)
+
+        # Opening file to encrypt
+        with open(self.file, "rb") as file:
+            original_file = file.read()
+
+        # Encrypting the file
+        encrypted = fernet.encrypt(original_file)
+
+        # Opening the file in write mode and writing the encrypted data
+        with open(f"{self.name}_encrypted{self.extension}", "wb") as encrypted_file:
+            encrypted_file.write(encrypted)

b528687b366dcf3518ddb71d6eaddc7fc4c270ad
MSProgrammerPy <None>
2021-04-06 15:05:01 +0430

    '_generate_key' function was added.

--- crypto.py
+++ crypto.py
@@ -3,10 +3,13 @@ import os
 
 
 class Crypto:
-    _FileName = None
-    _FileExtension = None
 
     def __init__(self, file: str):
         name, ext = os.path.splitext(file)
-        self._FileName = name
-        self._FileExtension = ext
+        self.file_name = name
+        self.file_extension = ext
+        self._key = None
+
+    def _generate_key(self):
+        key = Fernet.generate_key()
+        self._key = key

0137b41973cd8cf62d7e3108005483b6ce64100f
MSProgrammerPy <None>
2021-05-18 23:04:54 +0430

    Get the key file from the user if user wants to decrypt the file.

--- main.py
+++ main.py
@@ -4,26 +4,38 @@ from crypto import Crypto
 if len(sys.argv) >= 3:
     option = sys.argv[1]
     file = sys.argv[2]
-    crypto = Crypto(file)
+    crypto = Crypto(file=file)
 
     if option == "encrypt":
         crypto.encrypt()
 
     elif option == "decrypt":
-        crypto.decrypt()
+        try:
+            key = sys.argv[3]
+        except IndexError:
+            print(
+                "Please provide key file.\n\n"
+                "usage:\n"
+                "python main.py [encrypt|decrypt] <file> <key file>\n"
+                "key file: Optional. if you want to decrypt a file, you must provide it.\n"
+            )
+            sys.exit()
+        crypto.decrypt(key_file=key)
 
 else:
     print(
-        f"You can use:\n"
-        f"python main.py [encrypt|decrypt] <file>\n"
+        "You can also use:\n"
+        "python main.py [encrypt|decrypt] <file> <key file>\n"
+        "key file: Optional. if you want to decrypt a file, you must provide it.\n"
     )
 
     option = input("Choose an option [encrypt|decrypt]: ")
-    file = input("Enter file name: ")
+    file = input("Enter file path: ")
     crypto = Crypto(file)
 
     if option == "encrypt":
         crypto.encrypt()
 
     elif option == "decrypt":
-        crypto.decrypt()
+        key = input("Enter key file path: ")
+        crypto.decrypt(key_file=key)
